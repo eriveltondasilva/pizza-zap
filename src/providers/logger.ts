a7c8d8a68c40c5c870ba7a1610bd4adde8a02a27
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-04-22 09:45:33 -0300

    feat: implement custom logger with pino and file transport

---
+++ src/providers/logger.ts
@@ -0,0 +1,107 @@
+import { join } from 'node:path'
+import { env } from 'node:process'
+import { singleton } from 'tsyringe'
+import {
+  type Logger as WinstonLogger,
+  createLogger,
+  format,
+  transports,
+} from 'winston'
+
+type LogMeta = Record<string, unknown>
+
+interface ILogger {
+  info(message: string, meta?: LogMeta): void
+  ok(message: string, meta?: LogMeta): void
+  warn(message: string, meta?: LogMeta): void
+  debug(message: string, meta?: LogMeta): void
+  error(message: string, meta?: unknown): void
+}
+
+@singleton()
+export class LoggerProvider implements ILogger {
+  private readonly logger: WinstonLogger
+  private readonly logDir: string = 'logs'
+  private readonly maxFiles: number = 5
+  private readonly maxSize: number = 5 * 1_024 * 1_024 // 5MB
+  private readonly isProduction: boolean = env.NODE_ENV === 'production'
+
+  constructor() {
+    this.logger = this.createLogger()
+  }
+
+  // ###
+  public info(message: string, meta?: LogMeta): void {
+    this.logger.info(message, meta)
+  }
+
+  public ok(message: string, meta?: LogMeta): void {
+    this.logger.info(`✅ ${message}`, meta)
+  }
+
+  public warn(message: string, meta?: LogMeta): void {
+    this.logger.warn(`⚠️ ${message}`, meta)
+  }
+
+  public debug(message: string, meta?: LogMeta): void {
+    this.logger.debug(`⚙️ ${message}`, meta)
+  }
+
+  public error(message: string, meta?: unknown): void {
+    this.logger.error(`❌ ${message}`, meta)
+  }
+
+  //#
+  private createLogger(): WinstonLogger {
+    const logger = createLogger({
+        // defaultMeta: { service: SESSION_NAME },
+      level: this.isProduction ? 'info' : 'debug',
+      transports: this.createFileTransports(),
+    })
+
+    if (!this.isProduction) logger.add(this.createConsoleTransport())
+
+    return logger
+  }
+
+  private createFileTransports() {
+    const logFormat = format.combine(
+      format.errors(),
+      format.timestamp(),
+      format.metadata({
+        key: 'meta',
+        fillExcept: ['level', 'message', 'timestamp'],
+      }),
+      format.prettyPrint(),
+    )
+
+    return [
+      new transports.File({
+        level: 'error',
+        filename: join(this.logDir, 'error.log'),
+        format: logFormat,
+        maxsize: this.maxSize,
+        maxFiles: this.maxFiles,
+      }),
+      new transports.File({
+        level: 'info',
+        filename: join(this.logDir, 'combined.log'),
+        format: logFormat,
+        maxsize: this.maxSize,
+        maxFiles: this.maxFiles,
+      }),
+    ]
+  }
+
+  private createConsoleTransport() {
+    return new transports.Console({
+      format: format.combine(
+        format.errors(),
+        // format.prettyPrint(),
+        format.align(),
+        format.colorize({ all: true }),
+        format.simple(),
+      ),
+    })
+  }
+}
