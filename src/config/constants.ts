68c8fef7d20fb9a1f2503f171c8fdcb4ed0498e1
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-04-22 10:40:11 -0300

    feat: introduce application constants

---
+++ src/config/constants.ts
@@ -0,0 +1,9 @@
+import { env } from 'node:process'
+
+export const SESSION_NAME = env.SESSION_NAME || 'session-name'
+export const ERIVELTON_NUMBER = env.ERIVELTON_NUMBER || ''
+export const INGRID_NUMBER = env.INGRID_NUMBER || ''
+
+export const STATE_EXPIRATION_TIME = 1_000 * 60 * 60 * 24 // 24h
+export const CLEANUP_INTERVAL = 1_000 * 60 * 60 * 12 // 12h
+export const MAX_STATES = 1_000
