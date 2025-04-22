6f73accbd3b1d2ca4fdc2f638efc5753108e4b39
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-04-22 08:54:05 -0300

    feat: implement whatsapp message responder
    This commit introduces a basic WhatsApp message responder using the wppconnect library.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-const message: string = 'Hello World!'
+import { type Whatsapp, create } from '@wppconnect-team/wppconnect'
 
-console.log(message)
+create({
+  phoneNumber: '5582999723607',
+  disableWelcome: true,
+})
+  .then((client) => start(client))
+  .catch((error) => console.log(error))
+
+function start(client: Whatsapp) {
+  client.onAnyMessage((message) => {
+    if (message.body === 'Hello') {
+      client
+        .sendListMessage(message.from, {
+          buttonText: 'Click here',
+          description: 'Choose one option',
+          sections: [
+            {
+              title: 'Section 1',
+              rows: [
+                {
+                  rowId: 'my_custom_id',
+                  title: 'Test 1',
+                  description: 'Description 1',
+                },
+                {
+                  rowId: '2',
+                  title: 'Test 2',
+                  description: 'Description 2',
+                },
+              ],
+            },
+          ],
+        })
+        .then((result) => {
+          console.log('Result: ', result)
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro)
+        })
+    }
+  })
+}
