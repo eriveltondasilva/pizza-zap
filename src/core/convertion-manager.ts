309b8c1aa0acb5b155b8682d7ada8d08fc7c01a5
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-04-24 21:07:09 -0300

    feat: implement state facade and conversation manager

---
+++ src/core/convertion-manager.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from 'tsyringe'
+
+import { FlowFactory } from '@/flows/flow.factory.js'
+import { LoggerProvider } from '@/providers/logger.js'
+import { StateFacade } from './state.facade.js'
+
+import type { FlowResponse } from '@/types/flows.js'
+
+@injectable()
+export class ConversationManager {
+  constructor(
+    @inject(FlowFactory) private readonly flowFactory: FlowFactory,
+    @inject(StateFacade) private readonly state: StateFacade,
+    @inject(LoggerProvider) private readonly logger: LoggerProvider,
+  ) {}
+
+  public async handle(phone: string, message: string): Promise<FlowResponse> {
+    this.logger.info(`#️⃣ ${this.constructor.name}:`, { phone })
+
+    try {
+      const { context } = this.state.getState(phone)
+      this.logger.info(`🔄️ Processando fluxo: ((${context.flow}))`, { phone })
+
+      const flow = this.flowFactory.create(context.flow)
+      return await flow.handle({ context, phone, message })
+    } catch (error) {
+      this.logger.error('Erro durante o processamento do fluxo:', error)
+      this.state.deleteState(phone)
+      throw error
+    }
+  }
+}
---
+++ src/core/state.facade.ts
@@ -0,0 +1,91 @@
+import { inject, singleton } from 'tsyringe'
+
+import {
+  CartService,
+  ContextService,
+  CustomerService,
+  StateManager,
+} from '@/services/states/index.js'
+
+import type { FLOWS } from '@/config/enums.js'
+import type { CartItem, Customer } from '@/types/entities.js'
+import type { FlowContext, FlowData, FlowState } from '@/types/flows.js'
+
+@singleton()
+export class StateFacade {
+  constructor(
+    @inject(CartService) private readonly cartService: CartService,
+    @inject(ContextService) private readonly contextService: ContextService,
+    @inject(CustomerService) private readonly customerService: CustomerService,
+    @inject(StateManager) private readonly stateManager: StateManager,
+  ) {}
+
+  //# CONTEXT METHODS
+  public getState(phone: string): FlowState {
+    return this.stateManager.get(phone)
+  }
+
+  public updateContext(phone: string, context: Partial<FlowContext>): FlowState {
+    const state = this.getState(phone)
+    return this.contextService.updateContext(phone, state, context)
+  }
+
+  public updateFlow(phone: string, flow: FLOWS): FlowState {
+    const state = this.getState(phone)
+    return this.contextService.updateFlow(phone, state, flow)
+  }
+
+  public updateData(phone: string, data: FlowData): FlowState {
+    const state = this.getState(phone)
+    return this.contextService.updateData(phone, state, data)
+  }
+
+  public clearData(phone: string): FlowState {
+    const state = this.getState(phone)
+    return this.contextService.clearData(phone, state)
+  }
+
+  //# CUSTOMER METHODS
+  public updateCustomer(phone: string, customer: Partial<Customer>): FlowState {
+    const state = this.getState(phone)
+    return this.customerService.updateCustomer(phone, state, customer)
+  }
+
+  //# CART METHODS
+  public addToCart(phone: string, item: CartItem): FlowState {
+    const state = this.getState(phone)
+    return this.cartService.addToCart(phone, state, item)
+  }
+
+  public removeFromCart(phone: string, index: number): FlowState {
+    const state = this.getState(phone)
+    return this.cartService.removeFromCart(phone, state, index)
+  }
+
+  public clearCart(phone: string): FlowState {
+    const state = this.getState(phone)
+    return this.cartService.clearCart(phone, state)
+  }
+
+  // public getCartTotal(phone: string): number {
+  //   const state = this.getState(phone)
+  //   return this.cartService.getCartTotal(state)
+  // }
+
+  //# STATE MANAGEMENT
+  public hasState(phone: string): boolean {
+    return this.stateManager.has(phone)
+  }
+
+  public resetState(phone: string): FlowState {
+    return this.stateManager.reset(phone)
+  }
+
+  public deleteState(phone: string): boolean {
+    return this.stateManager.delete(phone)
+  }
+
+  public clearAllStates(): void {
+    this.stateManager.clearAllStates()
+  }
+}
